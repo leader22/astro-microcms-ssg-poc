158af19c9db4c6a695f37b4b976625d148f72357
Yuji Sugiura <y.sugiura.0316@gmail.com>
2022-03-23 17:07:51 +0900

    Fix site url

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,7 @@ import { defineConfig } from "astro/config";
 export default defineConfig({
   renderers: ["@astrojs/renderer-svelte"],
   buildOptions: {
-    site: "https://astro-ssg-poc.example.com",
+    site: "https://astro-microcms-ssg-poc.example.com",
     sitemapFilter: (url) => url.includes("/admin/") ? false : true,
   },
 });
