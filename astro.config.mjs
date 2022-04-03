f363c1e960f28f47eb28aac1cc06ecdc57258a79
Yuji Sugiura <y.sugiura.0316@gmail.com>
2022-04-03 20:52:16 +0900

    Migrate to astro@0.26

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,16 @@
 import { defineConfig } from "astro/config";
 import svelte from "@astrojs/svelte";
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [svelte()],
-  buildOptions: {
-    site: "https://astro-microcms-ssg-poc.example.com",
-    // Integrations also has @astrojs/sitemap but currently it does not use this filter!
-    sitemapFilter: url => url.includes("/admin/") ? false : true
-  },
+  integrations: [
+    svelte(),
+    sitemap({
+      filter(url) {
+        return url.includes("/admin/") ? false : true;
+      },
+    }),
+  ],
+  site: "https://astro-microcms-ssg-poc.example.com",
 });
